1659b5a0ddff033c9905dc7d66b33050dfb0e244
ziadhanii <UG_31107103@ics.tanta.edu.eg>
2025-02-16 19:43:24 +0200

    add user and task components with associated styles and models; include dummy user data and update index title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
+
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
 import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,
-    HeaderComponent],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [HeaderComponent, UserComponent, TasksComponent]
 })
 export class AppComponent {
-  title = 'first-angular-app';
+  users = DUMMY_USERS;
+  selectedUserId?: string;
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
+
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
+  }
 }
